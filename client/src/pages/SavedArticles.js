1d53b7e33268a6f71afbf26fe6f5b5ece5fd2152
Allen Welch <allenjwelch@gmail.com>
2018-06-27 18:38:51 -0400

    route pages opening successfully

---
+++ client/src/pages/SavedArticles.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SavedArticles = props => (
+  <h2>SavedArticles</h2>
+);
+
+export default SavedArticles;
--- client/src/pages/ScrapedArticles.js
+++ client/src/pages/ScrapedArticles.js
@@ -1,5 +1,11 @@
 import React from "react";
 // Require request and cheerio. This makes the scraping possible
-const request = require("request");
-const cheerio = require("cheerio");
+// const request = require("request");
+// const cheerio = require("cheerio");
 
+
+const ScrapedArticles = props => (
+  <h2>ScrapedArticles</h2>
+);
+
+export default ScrapedArticles; 
